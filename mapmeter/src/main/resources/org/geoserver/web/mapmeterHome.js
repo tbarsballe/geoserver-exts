ee44aef1f8bf219e965a7f10b5cb9180bfe6995b
Robert Marianski <rob@marianski.com>
2014-06-17 14:51:11 -0400

    work around basic auth dialog box
    When the user is anonymous and visits the home page, an ajax call was
    being made to fetch mapmeter data. The response was a basic auth
    challenge, and the browser would throw up a basic auth dialog box. This
    conditionally only makes that request if there is no login link in the
    header.

--- mapmeter/src/main/resources/org/geoserver/web/mapmeterHome.js
+++ mapmeter/src/main/resources/org/geoserver/web/mapmeterHome.js
@@ -1,8 +1,18 @@
 (function() {
   $(document).ready(function() {
-    var mapmeterDataContainer = $('<div></div>')
-      .attr('id', 'mapmeter-data-container');
-    $('.page-pane').append(mapmeterDataContainer);
-    mapmeter.fetchDataAndApply(mapmeterDataContainer);
+
+    // to prevent a basic auth dialog box from appearing,
+    // conditionally try to fetch mapmeter data only when user is logged in
+    // going forward, it would probably be better to handle this server side
+
+    // check for the login link to see if user is logged in or not
+    // this is a brittle way to handle this
+    if ($('#header .button-login').length === 0) {
+      var mapmeterDataContainer = $('<div></div>')
+        .attr('id', 'mapmeter-data-container');
+      $('.page-pane').append(mapmeterDataContainer);
+      mapmeter.fetchDataAndApply(mapmeterDataContainer);
+    }
+
   });
 }());
