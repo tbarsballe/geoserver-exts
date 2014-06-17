e9cdfa7646f62f36291d8e8b1755dcd0772182f3
Robert Marianski <rob@marianski.com>
2014-06-17 16:27:18 -0400

    Revert "work around basic auth dialog box"
    This reverts commit ee44aef1f8bf219e965a7f10b5cb9180bfe6995b.

--- mapmeter/src/main/resources/org/geoserver/web/mapmeterHome.js
+++ mapmeter/src/main/resources/org/geoserver/web/mapmeterHome.js
@@ -1,18 +1,8 @@
 (function() {
   $(document).ready(function() {
-
-    // to prevent a basic auth dialog box from appearing,
-    // conditionally try to fetch mapmeter data only when user is logged in
-    // going forward, it would probably be better to handle this server side
-
-    // check for the login link to see if user is logged in or not
-    // this is a brittle way to handle this
-    if ($('#header .button-login').length === 0) {
-      var mapmeterDataContainer = $('<div></div>')
-        .attr('id', 'mapmeter-data-container');
-      $('.page-pane').append(mapmeterDataContainer);
-      mapmeter.fetchDataAndApply(mapmeterDataContainer);
-    }
-
+    var mapmeterDataContainer = $('<div></div>')
+      .attr('id', 'mapmeter-data-container');
+    $('.page-pane').append(mapmeterDataContainer);
+    mapmeter.fetchDataAndApply(mapmeterDataContainer);
   });
 }());
