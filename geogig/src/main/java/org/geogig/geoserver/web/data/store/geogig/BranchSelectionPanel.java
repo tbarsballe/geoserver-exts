d49de7b26fb139f24573781e81f5cff325ad9ccd
David Winslow <cdwinslow@gmail.com>
2014-08-27 11:11:36 -0400

    Add branch selector dropdown to GeoGig datastore settings page

---
+++ geogig/src/main/java/org/geogig/geoserver/web/data/store/geogig/BranchSelectionPanel.java
@@ -0,0 +1,83 @@
+package org.geogig.geoserver.web.data.store.geogig;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;                                                                                                                       
+import org.apache.wicket.markup.html.form.DropDownChoice;                                                                                                                            
+import org.apache.wicket.markup.html.form.Form;                                                                                                                            
+import org.apache.wicket.markup.html.form.FormComponent;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+import org.geoserver.web.util.MapModel;
+import org.locationtech.geogig.api.Ref;
+import org.locationtech.geogig.api.porcelain.BranchListOp;
+import org.locationtech.geogig.geotools.data.GeoGigDataStoreFactory;
+import org.locationtech.geogig.geotools.data.GeoGigDataStore;
+
+public class BranchSelectionPanel extends Panel {
+    private static final long serialVersionUID = 1L; 
+
+    public static final String BRANCH_NAME = "master";
+    private final DropDownChoice choice;
+    private final FormComponent repositoryComponent;
+
+    public BranchSelectionPanel(String id, IModel paramsModel, Form storeEditForm, FormComponent repositoryComponent) {
+        super(id);
+        final MapModel branchNameModel = new MapModel(paramsModel, BRANCH_NAME);
+        final List<String> choices = new ArrayList<String>();
+        this.repositoryComponent = repositoryComponent;
+        choice = new DropDownChoice("branchDropDown", branchNameModel, choices);
+        choice.setOutputMarkupId(true);
+        choice.setNullValid(true);
+        choice.setRequired(true);
+        add(choice);
+
+        final AjaxSubmitLink refreshLink = new AjaxSubmitLink("refresh", storeEditForm) {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            protected void onError(AjaxRequestTarget target, Form form) {
+                onSubmit(target, form);
+            }
+
+            @Override
+            protected void onSubmit(AjaxRequestTarget target, Form form) {
+                final String repository = BranchSelectionPanel.this.repositoryComponent.getValue();
+                List<String> branchNames;
+                try {
+                    branchNames = getBranchNames(repository);
+                } catch (IOException e) {
+                    branchNames = Collections.emptyList();
+                } catch (RuntimeException e) {
+                    branchNames = Collections.emptyList();
+                }
+                choice.setChoices(branchNames);
+                target.addComponent(choice);
+            }
+        };
+        add(refreshLink);
+    }
+
+    private List<String> getBranchNames(String repository) throws IOException {
+        GeoGigDataStoreFactory factory = new GeoGigDataStoreFactory();
+        Map<String, Serializable> parameters = new HashMap<String, Serializable>();
+        parameters.put(GeoGigDataStoreFactory.REPOSITORY.key, repository);
+        GeoGigDataStore store = factory.createNewDataStore(parameters);
+        List<Ref> refs = store.getGeogig().command(BranchListOp.class).call();
+        List<String> names = new ArrayList<String>();
+        for (Ref r : refs) {
+            names.add(r.localName());
+        }
+        return names;
+    }
+
+    public FormComponent getFormComponent() {
+        return choice;
+    }
+}
---
+++ geogig/src/main/java/org/geogig/geoserver/web/data/store/geogig/GeoGigDataStoreEditPanel.java
@@ -0,0 +1,110 @@
+package org.geogig.geoserver.web.data.store.geogig;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+import org.apache.wicket.behavior.SimpleAttributeModifier;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.FormComponent;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.model.ResourceModel;
+import org.geoserver.catalog.DataStoreInfo;
+import org.geoserver.web.data.store.StoreEditPanel;
+import org.geoserver.web.data.store.panel.CheckBoxParamPanel;
+import org.geoserver.web.data.store.panel.DropDownChoiceParamPanel;
+import org.geoserver.web.data.store.panel.PasswordParamPanel;
+import org.geoserver.web.data.store.panel.TextParamPanel;
+import org.geoserver.web.util.MapModel;
+import org.geotools.data.DataAccessFactory.Param;
+import org.geotools.util.logging.Logging;
+import static org.locationtech.geogig.geotools.data.GeoGigDataStoreFactory.REPOSITORY;
+import static org.locationtech.geogig.geotools.data.GeoGigDataStoreFactory.BRANCH;
+import static org.locationtech.geogig.geotools.data.GeoGigDataStoreFactory.HEAD;
+import static org.locationtech.geogig.geotools.data.GeoGigDataStoreFactory.CREATE;
+
+public final class GeoGigDataStoreEditPanel extends StoreEditPanel {
+    private static final Logger LOGGER = Logging.getLogger("org.geogig.geoserver.web.data.store.geogig");
+    private static final String RESOURCE_KEY_PREFIX = GeoGigDataStoreEditPanel.class.getSimpleName();
+    final FormComponent repository;
+    final FormComponent branch;
+    final FormComponent create;
+
+    public GeoGigDataStoreEditPanel(final String componentId, final Form storeEditForm) {
+        super(componentId, storeEditForm);
+        final IModel model = storeEditForm.getModel();
+        setDefaultModel(model);
+        final DataStoreInfo storeInfo = (DataStoreInfo) storeEditForm.getModelObject();
+        {
+            Map<String, String> existingParameters = parseConnectionParameters(storeInfo);
+            Map<String, Serializable> connectionParameters = storeInfo.getConnectionParameters();
+            connectionParameters.putAll(existingParameters);
+        }
+        final IModel paramsModel = new PropertyModel(model, "connectionParameters");
+
+        addConnectionPrototypePanel(storeInfo);
+
+        repository = addTextPanel(paramsModel, REPOSITORY);
+        branch = addBranchNameComponent(paramsModel);
+        create = addCheckboxPanel(paramsModel, CREATE);
+    }
+
+    private Map<String, String> parseConnectionParameters(final DataStoreInfo storeInfo) {
+        Map<String, String> params = new HashMap<String, String>();
+        Map<String, Serializable> storeParams = storeInfo.getConnectionParameters();
+        params.put(REPOSITORY.key, (String) storeParams.get(REPOSITORY.key));
+        params.put(BRANCH.key, (String) storeParams.get(BRANCH.key));
+        params.put(CREATE.key, String.valueOf(storeParams.get(CREATE.key)));
+        return params;
+    }
+
+    private void addConnectionPrototypePanel(DataStoreInfo storeInfo) {
+        final String resourceKey = RESOURCE_KEY_PREFIX + ".prototype";
+        Label label = new Label("prototypeLabel", new ResourceModel(resourceKey));
+        final String title = String.valueOf(new ResourceModel(resourceKey + ".title").getObject());
+        final SimpleAttributeModifier titleSetter = new SimpleAttributeModifier("title", title);
+        label.add(titleSetter);
+        add(label);
+    }
+
+    private FormComponent addTextPanel(final IModel paramsModel, final Param param) {
+        final String paramName = param.key;
+        final String resourceKey = getClass().getSimpleName() + "." + paramName;
+        final boolean required = param.required;
+        final TextParamPanel textParamPanel = new TextParamPanel(paramName, new MapModel(paramsModel, paramName), new ResourceModel(resourceKey, paramName), required);
+        textParamPanel.getFormComponent().setType(param.type);
+        String defaultTitle = String.valueOf(param.title);
+        ResourceModel titleModel = new ResourceModel(resourceKey + ".title", defaultTitle);
+        String title = String.valueOf(titleModel.getObject());
+        textParamPanel.add(new SimpleAttributeModifier("title", title));
+        add(textParamPanel);
+        return textParamPanel.getFormComponent();
+    }
+
+    private FormComponent addCheckboxPanel(final IModel paramsModel, final Param param) {
+        final String paramName = param.key;
+        final String resourceKey = getClass().getSimpleName() + "." + paramName;
+        final boolean required = param.required;
+        final CheckBoxParamPanel checkboxParamPanel = new CheckBoxParamPanel(paramName, new MapModel(paramsModel, paramName), new ResourceModel(resourceKey, paramName));
+        checkboxParamPanel.getFormComponent().setType(param.type);
+        String defaultTitle = String.valueOf(param.title);
+        ResourceModel titleModel = new ResourceModel(resourceKey + ".title", defaultTitle);
+        String title = String.valueOf(titleModel.getObject());
+        checkboxParamPanel.add(new SimpleAttributeModifier("title", title));
+        add(checkboxParamPanel);
+        return checkboxParamPanel.getFormComponent();
+    }
+
+    private FormComponent addBranchNameComponent(final IModel paramsModel) {
+        final FormComponent branchNameComponent;
+        final String panelId = "branch";
+        BranchSelectionPanel selectionPanel;
+        selectionPanel = new BranchSelectionPanel(panelId, paramsModel, storeEditForm, repository);
+        add(selectionPanel);
+        return selectionPanel.getFormComponent();
+    }
+}
