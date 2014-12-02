409957dcf54a657f71d5fab7bf9165e2cb9a818f
jdeolive <jdeolive@boundlessgeo.com>
2014-12-02 16:19:40 -0700

    Adding NAMESPACE parameter to Mongo data store.
    GeoServer doesn't play nice with data stores that don't accept a namespace parameter.

--- mongodb/src/main/java/org/geotools/data/mongodb/MongoDataStoreFactory.java
+++ mongodb/src/main/java/org/geotools/data/mongodb/MongoDataStoreFactory.java
@@ -8,6 +8,7 @@ import org.geotools.data.DataStore;
 
 public class MongoDataStoreFactory extends AbstractDataStoreFactory {
 
+    public static final Param NAMESPACE = new Param("namespace", String.class, "Namespace prefix", false);
     public static final Param DATASTORE_URI = new Param("data_store", String.class, "MongoDB URI", true, "mongodb://localhost/<database name>");
     public static final Param SCHEMASTORE_URI = new Param("schema_store", String.class, "Schema Store URI", true, "file://<absolute path>");
     
@@ -23,13 +24,18 @@ public class MongoDataStoreFactory extends AbstractDataStoreFactory {
     
     @Override
     public Param[] getParametersInfo() {
-        return new Param[]{DATASTORE_URI, SCHEMASTORE_URI};
+        return new Param[]{NAMESPACE, DATASTORE_URI, SCHEMASTORE_URI};
     }
 
     @Override
     public MongoDataStore createDataStore(Map<String, Serializable> params) throws IOException {
-        return new MongoDataStore(
+        MongoDataStore dataStore = new MongoDataStore(
                 (String)DATASTORE_URI.lookUp(params), (String)SCHEMASTORE_URI.lookUp(params));
+        String uri = (String) NAMESPACE.lookUp(params);
+        if (uri != null) {
+            dataStore.setNamespaceURI(uri);
+        }
+        return dataStore;
     }
 
     @Override
